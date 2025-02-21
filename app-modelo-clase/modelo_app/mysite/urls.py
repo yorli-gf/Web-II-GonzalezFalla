cc3eb0ca6915c379c327775f6c7dc1394a1c9743
yorli gonzalez <yorligf08@gmail.com>
2025-02-21 16:08:40 -0600

    Correcciones

--- app-modelo-clase/modelo_app/mysite/urls.py
+++ app-modelo-clase/modelo_app/mysite/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
+from . import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("orders/", include("orders.urls")),
     path('users/', include('users.urls')),
+    path('', views.main, name="main")
 ]
---
+++ app-modelo-clase/modelo_app/mysite/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import get_object_or_404, render
+
+def main(request):
+    return render(request, "main/main.html")
