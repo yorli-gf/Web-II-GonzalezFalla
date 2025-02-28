1af6975d4407955eead5d0e8d48c6def4195fd21
yorli gonzalez <yorligf08@gmail.com>
2025-02-28 14:59:48 -0600

    HTML y CSS de Index, Boletos y Eventos

--- app-modelo-clase/modelo_app/examen/urls.py
+++ app-modelo-clase/modelo_app/examen/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index/', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('eventos/', views.eventos, name='eventos'),
     path('boletos/', views.boletos, name='boletos'),
 ]
--- app-modelo-clase/modelo_app/examen/views.py
+++ app-modelo-clase/modelo_app/examen/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 from .models import Evento, Boleto
 
 def index(request):
+    return render(request, 'examen/index.html')
+
+def index(request):
+    # Obtener los eventos destacados (puedes filtrar por algún criterio)
+    eventos_destacados = Evento.objects.all()[:3]  # Mostrar los primeros 3 eventos
+    return render(request, 'examen/index.html', {'eventos_destacados': eventos_destacados})
+
+def eventos(request):
     eventos = Evento.objects.all()
-    return render(request, 'examen/index.html', {'eventos': eventos})
+    return render(request, 'examen/eventos.html', {'eventos': eventos})
 
 def boletos(request):
     boletos = Boleto.objects.all()
